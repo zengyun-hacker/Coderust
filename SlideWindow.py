37f42d07925c91920ecc6a7b2f47e1f5941200fe
Programmer.Du <zengyun.hacker@gmail.com>
2020-03-14 22:00:26 +0800

    nlogN version of slide window

--- SlideWindow.py
+++ SlideWindow.py
@@ -1,28 +1,23 @@
 def find_max_sliding_window(arr, window_size):
   result = []
-  low = 0
-  high = window_size
-  while high <= len(arr):
-      tempArray = []
-      index = low
-      while index < high:
-        tempArray.append(arr[index])
-        index += 1
-      result.append(find_max_in_array(tempArray))
-      low += 1
-      high += 1
+  window = []
+  index = 0
+  while index < len(arr):
+    currentNumber = arr[index]
+    while len(window) > 0 and currentNumber > arr[window[len(window) - 1]]:
+        window.pop()
+    window.append(index)
+    if window[0] <= index - window_size:
+      window.pop(0)
+    if index >= window_size - 1:
+      result.append(arr[window[0]])
+    index += 1
 
   return result
 
-def find_max_in_array(array):
-  if len(array) == 0:
-    return None
-  max = array[0]
-  for number in array:
-    if number > max:
-      max = number
-  return max
 
 
 array = [-4, 2, -5, 3, 6]
-print(find_max_sliding_window(array, 3))
+array2 = []
+print(find_max_sliding_window([1, 2, 3, 4, 3, 2, 1, 2, 5],4))
+# print(find_max_sliding_window(array2, 3))
